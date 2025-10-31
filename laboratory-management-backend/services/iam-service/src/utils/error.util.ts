d4ba26b3044f923b0645277210cfc788e854b164
VTL-Repo <vuthienloct@gmail.com>
2025-10-31 08:28:08 +0700

    Small changes to error messaging

--- laboratory-management-backend/services/iam-service/src/utils/error.util.ts
+++ laboratory-management-backend/services/iam-service/src/utils/error.util.ts
@@ -2,12 +2,11 @@ import type { Response } from "express";
 
 export const errorHandler = (res: Response, error: any) => {
   const status = typeof error?.status === "number" ? error.status : 500;
-  const message = "Server Error!";
+  const message = error?.message || "Server Error!";
 
   console.error('Error details:', error);
 
   res.status(status).json({
-    message,
-    error: error?.error ?? message
+    error: error?.message ?? message
   });
 };
