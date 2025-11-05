007d00476626f698a2a773c9f1dc27660a416dce
VTL-Repo <vuthienloct@gmail.com>
2025-11-05 23:03:25 +0700

    Basic output change for register

--- laboratory-management-backend/services/iam-service/src/controllers/auth.controller.ts
+++ laboratory-management-backend/services/iam-service/src/controllers/auth.controller.ts
@@ -108,6 +108,7 @@ const registerUser = async (req: Request, res: Response, next: NextFunction): Pr
 
     res.status(200).json({
       message: "User created successfully!",
+      userId: newUser._id,
     });
   } catch (error) {
     next(error);
