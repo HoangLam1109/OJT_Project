26efbf1cd9cf26615b027d8e751d5255f74b7fb4
linhvo123 <vodinhlinh2058@gmail.com>
2025-11-05 10:58:40 +0700

    default table name for model IAM

--- laboratory-management-backend/services/iam-service/src/db/models/User.model.ts
+++ laboratory-management-backend/services/iam-service/src/db/models/User.model.ts
@@ -168,6 +168,6 @@ userSchema.pre("save", function (next) {
   next();
 });
 
-const UserModel = mongoose.model<IUser>("User", userSchema);
+const UserModel = mongoose.model<IUser>("User", userSchema, "users");
 
 export default UserModel;
