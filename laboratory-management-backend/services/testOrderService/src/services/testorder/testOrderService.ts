92e6cec9846da16269647319c210d1e925120517
VietHQ767 <vietquangthcs@gmail.com>
2025-11-15 13:21:33 +0700

    Revert "feat(backend): Add patient_id to test order search"
    This reverts commit 91ce8b341793f96a0ffaa02375de9b40680775f0.

--- laboratory-management-backend/services/testOrderService/src/services/testorder/testOrderService.ts
+++ laboratory-management-backend/services/testOrderService/src/services/testorder/testOrderService.ts
@@ -192,7 +192,6 @@ export const TestOrderService = {
       is_deleted: false,
       $or: [
         { patient_name: { $regex: keyword, $options: "i" } },
-        { patient_id: { $regex: keyword, $options: "i" } },
         { barcode: { $regex: keyword, $options: "i" } },
         { test_type: { $regex: keyword, $options: "i" } },
       ],
