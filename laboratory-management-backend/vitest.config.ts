520435c63f48e76a25ed276c7f56cf804152c8eb
VTL-Repo <vuthienloct@gmail.com>
2025-11-25 12:19:33 +0700

    Create more tests for iam service and notification service

--- laboratory-management-backend/vitest.config.ts
+++ laboratory-management-backend/vitest.config.ts
@@ -3,8 +3,16 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
-    include: [
-      "services/**/src/test/**/*.test.ts",
-    ],
+    include: ["services/**/src/test/**/*.test.ts"],
+    onConsoleLog(log, type) {
+      if (
+        log.includes("[NotifServiceClient]") ||
+        log.includes("OAuth status check error:") ||
+        log.includes("Error calling notification service createNotification")
+      ) {
+        return false;
+      }
+      return undefined;
+    },
   },
 });
