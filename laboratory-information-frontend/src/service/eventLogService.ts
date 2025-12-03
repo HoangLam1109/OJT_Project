0dfea1c7449d5f0ce4d841bbac4235f0b5f1f16b
phantrung208 <phantrungna23@gmail.com>
2025-12-03 23:42:12 +0700

    fix event log

--- laboratory-information-frontend/src/service/eventLogService.ts
+++ laboratory-information-frontend/src/service/eventLogService.ts
@@ -36,6 +36,12 @@ export interface EventLog {
   action?: string;
   event_message?: string;
   entity_id?: string;
+  entity_info?: {
+    entity_code?: string;
+    entity_name?: string;
+    entity_type?: string;
+    _id?: string;
+  };
   changed_fields?: string[];
   old_values?: unknown;
   new_values?: unknown;
