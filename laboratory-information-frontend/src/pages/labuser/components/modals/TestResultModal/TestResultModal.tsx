2c09fa61e61f7192cfa3d99295bf453a42588a0f
Võ Đình  Linh <vodinhlinh2058@gmail.com>
2025-12-04 05:10:11 +0000

    Revert "Merge branch 'feature/fix-bug-v2' into 'develop'"
    This reverts merge request !337

--- laboratory-information-frontend/src/pages/labuser/components/modals/TestResultModal/TestResultModal.tsx
+++ laboratory-information-frontend/src/pages/labuser/components/modals/TestResultModal/TestResultModal.tsx
@@ -5,8 +5,6 @@ import Badge from '@/components/common/badge';
 import { FileText, User, TestTube, Activity } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 
-//use in patient detail
-
 interface TestResultModalProps {
   isOpen: boolean;
   onClose: () => void;
