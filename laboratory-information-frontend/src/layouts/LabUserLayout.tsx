6b8c454b93ca4a0229b91c9c21f1c43eb5dc8098
linhvo123 <vodinhlinh2058@gmail.com>
2025-12-05 08:28:47 +0700

    fix translate

--- laboratory-information-frontend/src/layouts/LabUserLayout.tsx
+++ laboratory-information-frontend/src/layouts/LabUserLayout.tsx
@@ -131,7 +131,7 @@ const LabUserLayoutContent: React.FC<LabUserLayoutProps & { sidebarCollapsed: bo
         navigationItems={navigationItems}
         currentUserName={currentUser.name}
         currentUserAvatar={profile?.avatar}
-        currentUserRole={t('mangager.labUser')}
+        currentUserRole={t('manager.labUser')}
         currentPage={currentPage}
         sidebarCollapsed={sidebarCollapsed}
         setSidebarCollapsed={setSidebarCollapsed}
